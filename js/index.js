54c71e737dc658a30b6b91959149d9482157f3eb
betulakgul <betul.akgul44@gmail.com>
2026-01-16 16:44:52 +0300

    notlar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-console.log("hello");
+//! home sidebar start 
+const btnOpenSidebar = document.querySelector("#btn-menu"); 
+btnOpenSidebar.addEventListener("click",function(){ /*tıklandığında olay olsun */
+
+}); 
+console.log(btnOpenSidebar);
+//! home sidebar end 
