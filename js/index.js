19547ba6e53883eb9b26f505d5e1c050cbd7c270
betulakgul <betul.akgul44@gmail.com>
2026-03-27 00:32:38 +0300

    27.03.2026

--- js/index.js
+++ js/index.js
@@ -90,3 +90,11 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 //! slider end
+
+//! add product to localStroge
+async function getData(){
+  const photos = await fetch("../js/data.json");
+  const data = await photos.json();
+  console.log(data);
+}
+getData();
