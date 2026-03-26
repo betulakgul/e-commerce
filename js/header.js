55743cfc746337f4c3b18e200caa7ba6ba5f9c31
betulakgul <betul.akgul44@gmail.com>
2026-03-27 01:28:50 +0300

    27.03

---
+++ js/header.js
@@ -0,0 +1,9 @@
+export default function () { //default şeklinde tanımlarsak isim vermemize gerek yok, 
+// böylelikle index.js de bu fonk çağırırken istediğimiz ismi yazabiliyoruz, mesela orda Header ismini verdik
+  const a = 5;
+  const b = 10;
+
+  const toplam = a + b;
+
+  console.log(toplam);
+}
--- js/index.js
+++ js/index.js
@@ -92,9 +92,23 @@ function showSlides(n) {
 //! slider end
 
 //! add product to localStroge
+//! ÖNEMLİ UYARI
+//! visuale data.json dosyasını ekledikten sonra index.html sağ tık ->open with live server yapmamız lazım yoksa javasicript çalışmaz hata verir
+
 async function getData(){
-  const photos = await fetch("../js/data.json");
+  
+  const photos = await fetch('../js/data.json');
   const data = await photos.json();
   console.log(data);
+ 
+  data ? localStorage.setItem("products", JSON.stringify(data)) : [];  //data true ise set et, değilse boş array set et
+  
 }
 getData();
+
+const products = localStorage.getItem("products");
+
+
+import Header from "./header.js";
+
+Header();
